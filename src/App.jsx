75146c0be8a3f604f97d404d860765e70baf40ef
breaper03 <breaper2021@gmail.com>
2023-05-16 01:04:38 -0400

    fix?

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { GlobalProvider } from './context/GlobalState'
 
 // import Header from './components/Header'
 import { Balance } from './components/Balance'
-import TransactionForm from './components/transactions/TransactionForm'
-import TransactionList from './components/transactions/TransactionList'
-import IncomeExpenses from './components/IncomeExpenses'
-import TransactionCharts from './components/transactions/TransactionCharts'
+import {TransactionForm} from './components/transactions/TransactionForm'
+import {TransactionList} from './components/transactions/TransactionList'
+import {IncomeExpenses} from './components/IncomeExpenses'
+import {TransactionCharts} from './components/transactions/TransactionCharts'
 function App() {
 
   return (
